50801855b1551c81a8f9ce539a9552ce53510486
yexinhua <yexinhua@rtdata.cn>
2018-04-23 19:08:44 +0800

    修改gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -20,11 +20,11 @@ util=huajx-util
 if [ -d "$util" ] ; then
   echo "将"$util"提交到配置库"
   cd $util
-  git pull origin master
+  echo yes | git pull origin master
   echo "yes"
   git add .
   git commit -am "$msg"
-  git push -u origin master
+  echo yes | git push -u origin master
   cd ..
 fi
 
