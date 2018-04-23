23661e01af8b42fb54cf149e51ff183ee9306791
yexinhua <yexinhua@rtdata.cn>
2018-04-23 23:52:35 +0800

    Huajx-admin api

--- gitpush.sh
+++ gitpush.sh
@@ -7,7 +7,7 @@ git pull origin master
 msg="无"
 [ -n "$1" ] && msg=$1
 
-for module in bin huajx-core huajx-util ; do
+for module in bin huajx-core huajx-entity ; do
   if [ -d "$module" ] ; then
     echo "将 "$module" 提交到配置库"
     cd $module
@@ -18,6 +18,55 @@ for module in bin huajx-core huajx-util ; do
     cd ..
   fi
 done
+
+# entity
+if [ -d "huajx-entity" ] ; then
+  cd huajx-entity
+
+  for module in huajx-admin-entity ; do
+    if [ -d "$module" ] ; then
+      echo "将 "$module" 提交到配置库"
+      cd $module
+      git pull origin master
+      git add .
+      git commit -am "$msg"
+      git push -u origin master
+      cd ..
+    fi
+  done
+
+  echo "将 huajx-entity 提交到配置库"
+  cd huajx-admin
+  git pull origin master
+  git add .
+  git commit -am "$msg"
+  git push -u origin master
+  cd ..
+fi
+
+if [ -d "huajx-admin" ] ; then
+  cd huajx-admin
+
+  for module in huajx-admin-api huajx-admin-provider huajx-admin-clent ; do
+    if [ -d "$module" ] ; then
+      echo "将 "$module" 提交到配置库"
+      cd $module
+      git pull origin master
+      git add .
+      git commit -am "$msg"
+      git push -u origin master
+      cd ..
+    fi
+  done
+
+  echo "将 huajx-admin 提交到配置库"
+  cd huajx-admin
+  git pull origin master
+  git add .
+  git commit -am "$msg"
+  git push -u origin master
+  cd ..
+fi
 
 git add .
 
