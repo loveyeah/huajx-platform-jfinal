fffeee898fb91f5bda3ab20b15e9500ca8555b52
yexinhua <yexinhua@rtdata.cn>
2018-04-23 20:26:30 +0800

    添加bin

--- gitpush.sh
+++ gitpush.sh
@@ -7,9 +7,9 @@ git pull origin master
 msg="无"
 [ -n "$1" ] && msg=$1
 
-for module in huajx-core huajx-util ; do
+for module in huajx-core huajx-util bin ; do
   if [ -d "$module" ] ; then
-    echo "将"$module"提交到配置库"
+    echo "将 "$module" 提交到配置库"
     cd $module
     git pull origin master
     git add .
