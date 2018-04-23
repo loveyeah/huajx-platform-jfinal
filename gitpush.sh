98c5dd436bc33be1c514401cc37a6774e430be90
yexinhua <yexinhua@rtdata.cn>
2018-04-23 19:35:49 +0800

    修改gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -7,14 +7,6 @@ git pull origin master
 msg="无"
 [ -n "$1" ] && msg=$1
 
-git add .
-
-echo "git commit -am $msg"
-git commit -am "$msg"
-
-echo "git push -u origin master"
-git push -u origin master
-
 for module in huajx-core huajx-util ; do
   if [ -d "$module" ] ; then
     echo "将"$module"提交到配置库"
@@ -26,3 +18,12 @@ for module in huajx-core huajx-util ; do
     cd ..
   fi
 done
+
+git add .
+
+echo "git commit -am $msg"
+git commit -am "$msg"
+
+echo "git push -u origin master"
+git push -u origin master
+
