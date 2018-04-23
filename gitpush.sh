017a243bbb731da619c98cd624f18206cb72f49f
yexinhua <yexinhua@rtdata.cn>
2018-04-23 18:45:59 +0800

    修改gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -2,6 +2,8 @@
 
 echo "项目Git操作"
 
+git pull origin master
+
 msg="无"
 [ -n "$1" ] && msg=$1
 
@@ -13,5 +15,23 @@ git commit -am "$msg"
 echo "git push -u origin master"
 git push -u origin master
 
+util=./huajx-util
 
+if [-f "$util"]; then
+  cd $util
+  git pull origin master
+  git add .
+  git commit -am "$msg"
+  git push -u origin master
+fi
+
+core=./huajx-core
+
+if [-f "$core"]; then
+  cd $core
+  git pull origin master
+  git add .
+  git commit -am "$msg"
+  git push -u origin master
+fi
 
