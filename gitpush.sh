e6565b65025151a9d1185a8fbfd531d210c341af
yexinhua <yexinhua@rtdata.cn>
2018-04-23 18:39:01 +0800

    无

--- gitpush.sh
+++ gitpush.sh
@@ -2,6 +2,15 @@
 
 echo "项目Git操作"
 
+msg="无"
+[ -n "$var" ] && msg=$1
+
 git add .
 
-git commit -am "$1"
+echo "git commit -am $msg"
+git commit -am "$msg"
+
+echo "git push -u origin master"
+git push -u origin master
+
+
